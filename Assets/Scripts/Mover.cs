d68e96b7bcbbf6048caf3950aa3d6c21bfe64a0f
Bary U Pollack <bpolla01@baker.edu>
2025-05-24 17:13:01 -0700

    Added UJ for up and down

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3.0f;
+    [SerializeField] float moveSpeed = 2.0f;
+
     void Start()
     {
     }
@@ -15,9 +16,24 @@ public class Mover : MonoBehaviour
 
     void MovePlayer()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = 0;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue, yValue, zValue);
+        if (Input.GetKey(KeyCode.Alpha0)) {
+            Vector3 newPos = new Vector3(0f, 1.5f, 0f);
+            transform.position = newPos;
+        }
+        else if (Input.GetKey(KeyCode.U))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+            float yValue = 0;
+            float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            transform.Translate(xValue, yValue, zValue);
+        }
     }
 }
